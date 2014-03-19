f00eb3fd0c39f51081dfb7ca8011ad0d2c48ed6d
shizhongkun <shozhongkun@bflife.com>
2014-03-19 17:17:28 +0800

    lsjalk
    lkskkll

---
+++ 终极版 226/越野e族 2/Mall/MallController/MallViewController.h
@@ -0,0 +1,13 @@
+//
+//  MallViewController.h
+//  越野e族
+//
+//  Created by 史忠坤 on 14-3-19.
+//  Copyright (c) 2014年 soulnear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MallViewController : UIViewController
+
+@end
---
+++ 终极版 226/越野e族 2/Mall/MallController/MallViewController.m
@@ -0,0 +1,38 @@
+//
+//  MallViewController.m
+//  越野e族
+//
+//  Created by 史忠坤 on 14-3-19.
+//  Copyright (c) 2014年 soulnear. All rights reserved.
+//
+
+#import "MallViewController.h"
+
+@interface MallViewController ()
+
+@end
+
+@implementation MallViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
